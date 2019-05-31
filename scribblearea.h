d74bebbb1d661058834d5842809bab6722a49c30
Osama Kawish <osamakawish@gmail.com>
2019-05-31 10:28:50 -0400

    Branch for initiating scribble class. ScribbleArea documentation included.

--- scribblearea.h
+++ scribblearea.h
@@ -16,6 +16,7 @@ public:
 
     bool openImage(const QString &fileName);
     bool saveImage(const QString &fileName, const char* fileFormat);
+
     void setPenColor(const QColor &newColor);
     void setPenWidth(int newWidth);
 
@@ -28,10 +29,15 @@ public slots:
     void print();
 
 protected:
+    // Mouse events for implementing the drawing.
     void mousePressEvent(QMouseEvent* event) override;
     void mouseMoveEvent(QMouseEvent* event) override;
     void mouseReleaseEvent(QMouseEvent* event) override;
+
+    // For updating the scribble area
     void paintEvent(QPaintEvent* event) override;
+
+    // For ensuring that QImage size > Widget size at all times.
     void resizeEvent(QResizeEvent* event) override;
 
 private:
@@ -40,12 +46,14 @@ private:
     void drawLineTo(const QPoint &endPoint);
     void resizeImage(QImage* image, const QSize &newSize);
 
-    bool modified;
-    bool scribbling;
+    bool modified;      // True if file contains unsaved changes.
+    bool scribbling;    // True while the user is pressing the mouse for scribbling.
+
     int myPenWidth;
     QColor myPenColor;
-    QImage image;
-    QPoint lastPoint;
+
+    QImage image;       // Stores image drawn by user.
+    QPoint lastPoint;   // Last point the cursor moved/was pressed.
 };
 
 #endif // SCRIBBLEAREA_H
