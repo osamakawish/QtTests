650c623d35fb4df0722897a27964e25d147bfa5c
Osama Kawish <osamakawish@gmail.com>
2019-05-31 07:24:00 -0400

    Scribble Area header file finished. Should add documentation.

--- scribblearea.h
+++ scribblearea.h
@@ -13,10 +13,39 @@ class ScribbleArea : public QWidget
 
 public:
     explicit ScribbleArea(QWidget *parent = nullptr);
-    ~ScribbleArea();
+
+    bool openImage(const QString &fileName);
+    bool saveImage(const QString &fileName, const char* fileFormat);
+    void setPenColor(const QColor &newColor);
+    void setPenWidth(int newWidth);
+
+    bool isModified() const { return modified; }
+    QColor penColor() const { return myPenColor; }
+    int penWidth() const { return myPenWidth; }
+
+public slots:
+    void clearImage();
+    void print();
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void paintEvent(QPaintEvent* event) override;
+    void resizeEvent(QResizeEvent* event) override;
 
 private:
     Ui::ScribbleArea *ui;
+
+    void drawLineTo(const QPoint &endPoint);
+    void resizeImage(QImage* image, const QSize &newSize);
+
+    bool modified;
+    bool scribbling;
+    int myPenWidth;
+    QColor myPenColor;
+    QImage image;
+    QPoint lastPoint;
 };
 
 #endif // SCRIBBLEAREA_H
