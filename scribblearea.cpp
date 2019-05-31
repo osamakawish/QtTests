2ec8910ba4bad0387f3ca5a64d81106262d3867e
Osama Kawish <osamakawish@gmail.com>
2019-05-31 10:36:35 -0400

    Private members initiated in ScribbleArea constructor.

--- scribblearea.cpp
+++ scribblearea.cpp
@@ -6,9 +6,13 @@ ScribbleArea::ScribbleArea(QWidget *parent) :
     ui(new Ui::ScribbleArea)
 {
     ui->setupUi(this);
-}
 
-ScribbleArea::~ScribbleArea()
-{
-    delete ui;
+    // All contents remain at the top-left of the window, without resizing when wingow is scaled.
+    setAttribute(Qt::WA_StaticContents);
+
+    // Initiating private members.
+    modified = false;
+    scribbling = false;
+    myPenWidth = 1;
+    myPenColor = Qt::blue;
 }
