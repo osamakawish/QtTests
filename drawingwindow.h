8a5b49382c22ff6924c1f0dd161094b740e08b1f
Osama Kawish <osamakawish@gmail.com>
2019-06-03 09:56:08 -0400

    CLASS 'DrawingWindow' header file complete

--- drawingwindow.h
+++ drawingwindow.h
@@ -2,6 +2,7 @@
 #define DRAWINGWINDOW_H
 
 #include <QMainWindow>
+#include "scribblearea.h"
 
 namespace Ui {
 class DrawingWindow;
@@ -31,6 +32,30 @@ private slots:
 
 private:
     Ui::DrawingWindow *ui;
+
+    // Not sure about these methods purposes yet except saveFile.
+    void createActions();
+    void createMenus();
+    bool maybeSave();
+    bool saveFile(const QByteArray &fileFormat);
+
+    // The scribble area.
+    ScribbleArea *scribblearea;
+
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+
+    QAction *openAct;
+    QList<QAction *> saveAsActs;
+    QAction *exitAct;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *clearScreenAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
 };
 
 #endif // DRAWINGWINDOW_H
